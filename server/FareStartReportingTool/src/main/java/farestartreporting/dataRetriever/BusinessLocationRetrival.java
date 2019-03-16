3ac537d876688555ddccb9f1b3dbeebbae503d7a
J Hershey <hersheyj32@gmail.com>
2019-03-16 14:18:33 -0700

    Steven integrate with HTTP Client

---
+++ server/FareStartReportingTool/src/main/java/farestartreporting/dataRetriever/BusinessLocationRetrival.java
@@ -0,0 +1,13 @@
+package farestartreporting.dataRetriever;
+
+import farestartreporting.responseModel.BusinessLocation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusinessLocationRetrival {
+
+    public List<BusinessLocation> getReport(int locationGroupID, String dateOfBusiness){
+        return new ArrayList<>();
+    }
+}
