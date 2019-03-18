3ae5cb953d553a54e4bc91008424103c49a4cf1c
J Hershey <hersheyj32@gmail.com>
2019-03-18 12:41:56 -0700

    profiling

--- server/FareStartReportingTool/src/main/java/farestartreporting/getDataController.java
+++ server/FareStartReportingTool/src/main/java/farestartreporting/getDataController.java
@@ -15,6 +15,24 @@ import java.util.List;
 @RestController
 public class getDataController {
 
+    public static void main(String[] args) throws ParseException, IOException {
+        System.out.println("hello");
+        String startDate = "03-11-2019";
+        int dateRange = 1;
+        long start = System.currentTimeMillis();
+        System.out.println("Time now " + start);
+
+        getWeeklyHelper(startDate, dateRange);
+
+
+        long end = System.currentTimeMillis();
+        System.out.println("Time end " + end);
+        long totalTime = end - start;
+        System.out.println("API took: " + totalTime);
+
+
+    }
+
     @RequestMapping(value = "/api/getData", method = RequestMethod.GET, produces = "application/json")
     public BusinessResponse getData(@RequestParam String startDate, @RequestParam(required = true, defaultValue = "1") String range) throws IOException, ParseException {
 
@@ -27,7 +45,11 @@ public class getDataController {
             dateRange = 7;
         }
 
-        ArrayList<BusinessReport> businessReports = new ArrayList<>();
+
+        return getWeeklyHelper(startDate, dateRange);
+    }
+
+    public static BusinessResponse getWeeklyHelper(String startDate, int dateRange) throws ParseException, IOException {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd-yyyy");
         Date dateToWorkWith = simpleDateFormat.parse(startDate);
         Calendar calendar = Calendar.getInstance();
