e2b345eb360dc813f103abe2efc89bfb7af8c6bc
J Hershey <hersheyj32@gmail.com>
2019-03-19 20:20:26 -0700

    another place to add cache

--- server/FareStartReportingTool/src/main/java/farestartreporting/reporting/model/CacheService.java
+++ server/FareStartReportingTool/src/main/java/farestartreporting/reporting/model/CacheService.java
@@ -7,6 +7,7 @@ import farestartreporting.responseModel.BusinessReport;
 
 
 import java.io.IOException;
+import java.text.ParseException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 
@@ -21,4 +22,16 @@ public class CacheService {
                         }
                     }
             );
+
+    public static Cache<String, WeeklyReport> weeklyReportCache = CacheBuilder.newBuilder()
+            .maximumSize(1000)
+            .expireAfterAccess(24, TimeUnit.HOURS)
+            .build(
+                    new CacheLoader<String, WeeklyReport>() {
+                        public WeeklyReport load(String startDate) throws InterruptedException, ExecutionException, IOException, ParseException {
+                            return new WeeklyReport(startDate);
+                        }
+                    }
+            );
+
 }
