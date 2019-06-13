93bebef2d5d766080db30d2aba676cfc0a7576de
Andrew Siew <andrewsiew2@gmail.com>
2019-06-12 20:59:36 -0400

    Improved checkbox implementation

--- client/src/Components/OverLapOptions.js
+++ client/src/Components/OverLapOptions.js
@@ -1,50 +1,72 @@
 import React, { Component } from "react";
+import Checkbox from "./Checkbox";
 /**
  * This is meant to be a menu of checkboxes where the client can opt for additional information about
  * the selected time frame to be shown.
  */
+const OPTIONS = ["budget", "laborCost", "netSales"];
 class OverLapOptions extends Component {
-  constructor(props) {
-    super();
-  }
+    constructor(props) {
+        super();
+    }
 
-  render() {
-    const Checkbox = props => (
-      <input type="checkbox" {...props} />
+    
+
+    state = {
+        checkboxes: OPTIONS.reduce(
+          (options, option) => ({
+            ...options,
+            [option]: false
+          }),
+          {}
+        )
+    };
+
+    handleCheckboxChange = changeEvent => {
+        const { name } = changeEvent.target;
+
+        this.setState(prevState => ({
+            checkboxes: {
+            ...prevState.checkboxes,
+            [name]: !prevState.checkboxes[name]
+            }
+        }));
+
+    };
+
+    createCheckbox = option => (
+    <Checkbox
+        label={parseDataTypeName(option)}
+        isSelected={this.state.checkboxes[option]}
+        onCheckboxChange={this.handleCheckboxChange}
+        key={option}
+        name={option}
+    />
     );
 
+    createCheckboxes = () => OPTIONS.map(this.createCheckbox);
+
+  render() {
+
     return (
-      <div>
-        <h3> Overlapping Options </h3>
-        <label>
-          <Checkbox
-            checked={this.props.flag[0]}
-            onChange={() => this.props.func(!this.props.flag[0], this.props.flag[1], this.props.flag[2])}
-            value={0}
-          />
-          <span>Budget</span>
-        </label>
-        <br></br>
-        <label>
-          <Checkbox
-            checked={this.props.flag[1]}
-            onChange={() => this.props.func(this.props.flag[0], !this.props.flag[1], this.props.flag[2])}
-            value={1}
-          />
-          <span>Labor Cost</span>
-        </label>
-        <br></br>
-        <label>
-          <Checkbox
-            checked={this.props.flag[2]}
-            onChange={() => this.props.func(this.props.flag[0], this.props.flag[1], !this.props.flag[2])}
-            value={2}
-          />
-          <span>Net Sales</span>
-        </label>
-      </div>
+        <div className="container">
+            <h3>Overlap Options</h3>
+            <div className="row mt-5">
+                <div className="col-sm-12">
+                    {console.log()}
+                    {this.createCheckboxes()}
+                </div>
+            </div>
+        </div>
     );
   }
 }
+
+function parseDataTypeName(dataType){
+    dataType = dataType.charAt(0).toUpperCase() + dataType.slice(1); // sets first letter uppercase
+    let cleanName = dataType.match(/[A-Z][a-z]+/g); // this seperates the words by UpperCase letters
+    cleanName = cleanName.join(" "); //  joins them with a space
+    return cleanName;
+  }
 
 export default OverLapOptions;
